01d4fe7c55a774807422543f7482bbf5f8ffe0c6
Jordi Marques <jordi.marqu@gmail.com>
2023-03-03 09:52:10 +0100

    - Añadido buscar usuarios

--- usuarios/src/main/java/com/inicio/controllers/UserController.java
+++ usuarios/src/main/java/com/inicio/controllers/UserController.java
@@ -3,6 +3,9 @@ package com.inicio.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +27,14 @@ public class UserController {
 		service.altaUser(user);
 		
 		return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
+	
+	@GetMapping("/verUser/{user}")
+	ResponseEntity<?> verUser(@PathVariable String user){
+		
+		UserDetails usuario = service.getUser(user);
+		
+		return ResponseEntity.status(HttpStatus.OK).body(usuario);
 	}
 
 }
