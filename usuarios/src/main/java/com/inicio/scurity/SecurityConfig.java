6e1b285c085ecc493194746f4329e025bdda6803
Jordi Marques <jordi.marqu@gmail.com>
2023-03-03 09:05:48 +0100

    Inicio

---
+++ usuarios/src/main/java/com/inicio/scurity/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.inicio.scurity;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	
+ 	@Bean
+	public JdbcUserDetailsManager usersDetailsJdbc(DataSource dataSource) {
+		JdbcUserDetailsManager jdbcDetails = new JdbcUserDetailsManager();
+		jdbcDetails.setDataSource(dataSource);
+		jdbcDetails.setUsersByUsernameQuery("select username, password, enabled" + " from users where username=?");
+		jdbcDetails.setAuthoritiesByUsernameQuery("select username, authority " + "from authorities where username=?");
+			
+		return jdbcDetails;
+	}
+
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+		http.csrf().disable().authorizeRequests()
+				.antMatchers("/**").authenticated().and().httpBasic();
+
+		return http.build();
+	}
+	
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ usuarios/src/main/java/com/inicio/service/UserServiceImpl.java
@@ -0,0 +1,50 @@
+package com.inicio.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.stereotype.Service;
+import org.springframework.security.core.userdetails.User;
+
+
+@Service
+public class UserServiceImpl {
+	
+	@Autowired
+	PasswordEncoder passwordEncoder;
+
+	@Autowired
+	private UserDetailsManager userDetailsManager;
+	
+	
+	public void altaUser(com.inicio.models.User usuario) {
+		
+		UserDetails user = User.withUsername(usuario.getUsername()).password(passwordEncoder.encode(usuario.getPassword())).authorities(usuario.getRol()).build();
+		userDetailsManager.createUser(user);
+		
+	}
+	
+	public void modifUser(com.inicio.models.User usuario) {
+		
+		UserDetails user = User.withUsername(usuario.getUsername()).password(passwordEncoder.encode(usuario.getPassword())).authorities(usuario.getRol()).build();
+		userDetailsManager.updateUser(user);
+		
+	}
+	
+	public void borraUser(String username) {
+		
+		userDetailsManager.deleteUser(username);
+	}
+	
+	public boolean ifExist(String usuario) {
+		
+		
+		return userDetailsManager.userExists(usuario);
+	}
+	
+	public UserDetails getUser(String username) {
+		
+		return userDetailsManager.loadUserByUsername(username);
+	}
+}
