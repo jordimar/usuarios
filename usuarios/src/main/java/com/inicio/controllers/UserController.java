a2e3d5422d9aafc785f076466ec99ebd6428bb06
Jordi Marques <jordi.marqu@gmail.com>
2023-03-08 19:56:42 +0100

    - nueva rama repoUser2

--- usuarios/src/main/java/com/inicio/controllers/UserController.java
+++ usuarios/src/main/java/com/inicio/controllers/UserController.java
@@ -73,7 +73,7 @@ public class UserController {
 	@PutMapping("/modif")
 	ResponseEntity<?> modif(@RequestBody UserDto user){
 		
-		service.modifUser(user);
+		service.modifUser(user); 
 		
 		return ResponseEntity.ok().build();
 	}
